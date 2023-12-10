95459b2df152bfdd7da7aede94e4c754009049e2
Paul Bartolomeu <paul.bartolomeu@razielhealth.com>
2023-12-10 11:57:10 -0500

    Add project files.

---
+++ BiampTest/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BiampTest
+{
+    internal class Program
+    {
+        public static TcpTestServer ampdevice;
+        private static int volume = 0;
+        private static Int32 port = 6000;
+        private static IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+        //public delegate void DataArrivedHandler(object sender, DataArrivedEventArgs e);
+        //public event DataArrivedHandler DataArrived;
+        static void Main(string[] args)
+        {
+            bool EXIT = false;
+
+            TcpTestServer ampdevice = new TcpTestServer(port);
+            ampdevice.Start();
+            ampdevice.DataArrived += new TcpTestServer.DataArrivedHandler(ampdevice_DataArrived);
+
+            WriteTOConsole();
+            while (!EXIT)
+            {
+                ConsoleKeyInfo dd = Console.ReadKey();
+                switch(dd.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        {
+                            Send("increment");
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            Send("decrement");
+                            break;
+                        }
+                    case ConsoleKey.E:
+                        {
+                            EXIT = true;
+                            break;
+                        }
+                }
+
+            }
+
+            ampdevice.Stop();
+            Environment.Exit(0);
+            
+        }
+
+        static void ampdevice_DataArrived(object sender, DataArrivedEventArgs e)
+        {
+            var recData = e.Data;
+            string dataReceived = Encoding.ASCII.GetString(recData, 0, recData.Length);
+            if (dataReceived.Contains("increment"))
+            {
+                SetTheVolume(1);
+            }
+            else if (dataReceived.Contains("decrement")){
+                SetTheVolume(-1);
+            }
+
+            WriteTOConsole();
+        }
+
+        private static void Send(string inCvolume)
+        {
+            string message = $"Level1 {inCvolume} level 1 1" + Environment.NewLine;
+
+            TcpClient client = new TcpClient();
+            client.Connect(localAddr.ToString(), port);
+
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+            NetworkStream nwStream = client.GetStream();
+            nwStream.Write(data, 0, data.Length);
+
+            byte[] bytesToRead = new byte[data.Length];
+        }
+
+        private static void SetTheVolume(int newvolume) {
+            if (volume + newvolume > 10) {
+                volume = 10;
+            }
+            else if (volume + newvolume < 0)
+            {
+                volume = 0;
+            }
+            else
+            {
+                volume += newvolume;
+            }
+
+        }
+        private static void WriteTOConsole()
+        {
+            Console.Clear();
+            Console.WriteLine("Press up or down keys to increase or decrease volume respectively. Press E to stop program.");
+            Console.WriteLine(@$"Current Volume:{volume}");
+        }
+
+    }
+
+
+}
---
+++ BiampTest/TcpTestServer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BiampTest
+{
+    public class TcpTestServer : IDisposable
+    {
+
+        private const int BUFFER_SIZE = 1048;
+
+        public delegate void DataArrivedHandler(object sender, DataArrivedEventArgs e);
+        public event DataArrivedHandler DataArrived;
+
+        private TcpListener _listener = null;
+        private Thread _worker = null;
+        private string _ipAddress = "127.0.0.1";
+        private int _port = -1;
+
+        public TcpTestServer(int port) : this("127.0.0.1", port)
+        {
+
+        }
+
+        public TcpTestServer(string ipAddress, int port)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+
+        public void Start()
+        {
+            Close();
+            _listener = new TcpListener(IPAddress.Parse(_ipAddress), _port);
+
+            _worker = new Thread(new ThreadStart(DoWork));
+            _worker.Start();
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void Close()
+        {
+            Dispose();
+        }
+
+        protected void OnDataArrived(DataArrivedEventArgs e)
+        {
+            if (DataArrived != null)
+                DataArrived(this, e);
+        }
+
+        public void DoWork()
+        {
+
+            _listener.Start();
+
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = _listener.AcceptTcpClient();
+                    NetworkStream s = client.GetStream();
+                    byte[] buffer = new byte[BUFFER_SIZE];
+                    MemoryStream ms = new MemoryStream();
+
+                    int totalCount = 0;
+                    while (s.DataAvailable)
+                    {
+                        int count = s.Read(buffer, 0, buffer.Length);
+                        totalCount += count;
+                        if (count == 0)
+                            break;
+                        ms.Write(buffer, 0, count);
+                    }
+
+                    if (totalCount > 0)
+                        OnDataArrived(new DataArrivedEventArgs(ms.ToArray()));
+
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            try
+            {
+                if (_listener != null)
+                {
+                    _listener.Stop();
+                    _listener = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // ignore 
+            }
+
+            try
+            {
+                if (_worker != null)
+                {
+                    _worker.Abort();
+                    _worker = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // ignore 
+            }
+
+        }
+
+        #endregion
+    }
+
+
+    public class DataArrivedEventArgs : EventArgs
+    {
+
+        private byte[] _data = null;
+
+        public byte[] Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        public DataArrivedEventArgs(byte[] data)
+        {
+            _data = data;
+        }
+
+
+    }
+}
